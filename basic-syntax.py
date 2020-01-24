0d0c77a63fbb4c3c37e81c211815193f142f98f2
Md. Asaduzzaman Noor <md.noor.asad@gmail.com>
2020-01-24 23:48:01 +0600

    Create basic-syntax file.

---
+++ basic-syntax.py
@@ -0,0 +1,21 @@
+print("Hello, World!")
+
+#Data structrues - assume the gap as run command
+var1 = "sam"
+var1
+
+var1 = 'cool'
+var1
+
+#so variables are just temporary storage basis
+
+#every variable is associated with a data-type
+var1 = 12
+type(var1)
+
+var1 = True //not true or TRUE
+type(var1)
+
+var1 = 4 + 8j
+type(var1) //prints complex
+
