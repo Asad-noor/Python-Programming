c4321d109652d0b0934b32ee2fa04bdd51aa2ad9
Md. Asaduzzaman Noor <md.noor.asad@gmail.com>
2020-01-25 12:54:58 +0600

    Update basic syntax file with string handling.

--- basic-syntax.py
+++ basic-syntax.py
@@ -19,3 +19,28 @@ type(var1)
 var1 = 4 + 8j
 type(var1) //prints complex
 
+#String manipulation
+var1 = "Hello, World!"
+var1[0] //prints 'H'
+var1[-1] //prints '!'
+var1[-13] //prints 'H'
+
+len(var1) //prints 13
+
+var1[1:12] //prints 'ello, World' substring
+
+var1.upper() //prints 'HELLO, WORLD!'
+
+var1 + " gg" //string concat prints 'Hello, World! gg'
+
+var1 * 2 //string multiply prints 'Hello, World!Hello, World!'
+
+'example '.strip() //works as trim prints 'example'
+
+'This string has five words'.split() //spilt blank prints ['This', 'string', 'has', 'five', 'words']
+
+'one,two,three,four,five'.split(',') //comma separated to list conversion prints ['one', 'two', 'three', 'four', 'five']
+
+' '.join(['This', 'string', 'has', 'five', 'words']) //join space -> 'This string has five words'
+
+','.join(['one', 'two', 'three', 'four', 'five']) //join comma -> 'one,two,three,four,five'
